9de11aa7b72d536f7af2e5a59a2e6e23a6a5df09
Aidan Melen <aidan.melen94@gmail.com>
2016-03-30 15:27:30 -0600

    changed the autopilot algorithm. made it dumber and more simple, which, in turn, is more robust

--- workspace/CollisionAvoidance/CollisionAvoidance.ino
+++ workspace/CollisionAvoidance/CollisionAvoidance.ino
@@ -10,7 +10,7 @@
  * setup()
  * void printReceiverInputToConsole()
  * void printArduinoOutputToConsole()
- * boolean checkAuxForAutoPilotSignal()
+ * boolean isAutoPilotMode()
  * void forwardAuxSignal()
  * void forwardRCSignalsToFlightControllerToFlightController()
  * int runAutoPilotAndReturnClosest()
@@ -21,10 +21,11 @@
 
 #include <NewPing.h>
 
-#define OUTER_DISTANCE_THRESHOLD_FOR_SENSORS_IN_CM 80 // 1.5 ft
+#define OUTER_DISTANCE_THRESHOLD_FOR_SENSORS_IN_CM 150
+#define INNER_DISTANCE_THRESHOLD_FOR_SENSORS_IN_CM 10
 #define MAX_PING_DISTANCE_IN_CM 200
-#define NUMBER_OF_SONAR_BURSTS 5
-#define CAPACITY_OF_AUTOPILOT_MOVEMENTS 100
+#define NUMBER_OF_SONAR_BURSTS 10
+#define MAX_AUTOPILOT_MOVEMENT 100
 
 boolean chooseRandomSafetyDirection = false;
 boolean serialMonitorIsOpen = false;
@@ -69,15 +70,15 @@ NewPing p1(s1, s1, 200); // pins share the same for both trig and echo
 NewPing p2(s2, s2, 200);
 NewPing p3(s3, s3, 200);
 NewPing p4(s4, s4, 200);
-NewPing sensors[] = {p1, p2, p3, p4};
+NewPing sensorArray[] = {p1, p2, p3, p4};
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 // Setup
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 void setup() {
-  if (serialMonitorIsOpen == true) {
-    Serial.begin(115200);
-  }
+  //  if (serialMonitorIsOpen == true) {
+  Serial.begin(115200);
+  //  }
   pinMode(throttleIn, INPUT);
   pinMode(rollIn, INPUT);
   pinMode(pitchIn, INPUT);
@@ -90,7 +91,7 @@ void setup() {
   pinMode(yawOut, OUTPUT);
   pinMode(auxOut, OUTPUT);
 
-//  randomSeed(analogRead(0)); // for random safety direction
+  //  randomSeed(analogRead(0)); // for random safety direction
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -119,23 +120,138 @@ void printReceiverInputToConsole() {
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Print Sonar Measurements to Console
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+void printSonarMeasurementsToConsole(int sensorIndex, int distanceMeasurement) {
+  Serial.print("sensor: ");
+  Serial.print(sensorIndex);
+  Serial.print(" ");
+  Serial.print("distance: ");
+  Serial.print(distanceMeasurement);
+  Serial.print("  --  ");
+}
+
+void printFinalAvoidanceDirecton(int avoidanceDirection) {
+  if (avoidanceDirection == 0) {
+    Serial.println();
+    Serial.println("Avoidance Movement Left");
+    Serial.println();
+  }
+  if (avoidanceDirection == 0) {
+    Serial.println();
+    Serial.println("Avoidance Movement Front");
+    Serial.println();  
+  }
+  if (avoidanceDirection == 0) {
+    Serial.println();
+    Serial.println("Avoidance Movement To Right");
+    Serial.println();    
+  }
+  if (avoidanceDirection == 0) {
+    Serial.println();
+    Serial.println("Avoidance Movement To Backwards");
+    Serial.println();    
+  }
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 // Check Aux For AutoPilot Signal
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-boolean checkAuxForAutoPilotSignal() {
+boolean isAutoPilotMode() {
   aux = pulseIn(auxIn, HIGH);
   if (aux > 1500) return true;
   else return false;
 }
-
-
-
-
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 // Forward Aux Signal To The Flight Controller
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 void forwardAuxSignal() {
   analogWrite(auxOut, map(aux, 0, 2000, 0, 255));
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Forward RC Signals To Flight Controller
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+void forwardRCSignalsToFlightController() {
+  throttle = pulseIn(throttleIn, HIGH);
+  roll = pulseIn(rollIn, HIGH);
+  pitch = pulseIn(pitchIn, HIGH);
+  yaw = pulseIn(yawIn, HIGH);
+
+  analogWrite(throttleOut, map(throttle, 0, 2000, 0, 255));
+  analogWrite(rollOut, map(roll, 0, 2000, 0, 255));
+  analogWrite(pitchOut, map(pitch, 0, 2000, 0, 255));
+  analogWrite(yawOut, map(yaw, 0, 2000, 0, 255));
+
+  if (serialMonitorIsOpen) printReceiverInputToConsole();
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Run Auto Pilot Loop: determine sensor with closest measurement
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+int runAutoPilot() {
+  unsigned long finalDistanceMeasurement;
+  int potentialAvoidanceDirecton;
+  while (true) {
+    potentialAvoidanceDirecton = getClosestDirection();
+    
+    if (potentialAvoidanceDirecton != -1) {
+      /* double check measurement */
+      finalDistanceMeasurement = sensorArray[potentialAvoidanceDirecton].ping_median(NUMBER_OF_SONAR_BURSTS) / US_ROUNDTRIP_CM;
+      /* perform collison avoidance maneuver */
+      if (distanceMeasurementIsInRange(finalDistanceMeasurement)) forwardCollisionAvoidanceManeuverToFlightController((potentialAvoidanceDirecton + 2) % 4);
+    }
+    if (!isAutoPilotMode()) break;
+  }
+}
+
+int getClosestDirection() {
+  unsigned long initialDistanceMeasurement;
+  int potentialAvoidanceDirecton = -1;
+  for (int sensorIndex = 0; sensorIndex < 4; sensorIndex++)  {
+    forwardRCSignalsToFlightController();
+    initialDistanceMeasurement = sensorArray[sensorIndex].ping() / US_ROUNDTRIP_CM;
+    forwardRCSignalsToFlightController();
+
+    if (distanceMeasurementIsInRange(initialDistanceMeasurement)) potentialAvoidanceDirecton = sensorIndex;
+    if (!isAutoPilotMode()) break;
+  }
+  return potentialAvoidanceDirecton;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Check If Distance Measurement is in the target Range
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+boolean distanceMeasurementIsInRange(int distanceMeasurement) {
+  if (distanceMeasurement < OUTER_DISTANCE_THRESHOLD_FOR_SENSORS_IN_CM && distanceMeasurement > INNER_DISTANCE_THRESHOLD_FOR_SENSORS_IN_CM && distanceMeasurement != 0) return true;
+  else return false;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Forward Collision Avoidance Movement To The Flight Controler
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+void forwardCollisionAvoidanceManeuverToFlightController(int avoidanceDirection) {
+  switch (avoidanceDirection) {
+    case 0:
+      if (serialMonitorIsOpen) printFinalAvoidanceDirecton(avoidanceDirection);
+      smoothAcceleration(rollOut, roll, roll + MAX_AUTOPILOT_MOVEMENT); // move left
+      break;
+    case 1:
+      if (serialMonitorIsOpen) printFinalAvoidanceDirecton(avoidanceDirection);
+      smoothAcceleration(pitchOut, pitch, pitch + MAX_AUTOPILOT_MOVEMENT); // move forward
+      break;
+    case 2:
+      if (serialMonitorIsOpen) printFinalAvoidanceDirecton(avoidanceDirection);
+      smoothDeceleration(rollOut, roll, roll - MAX_AUTOPILOT_MOVEMENT); // move right
+      break;
+    case 3:
+      if (serialMonitorIsOpen) printFinalAvoidanceDirecton(avoidanceDirection);
+      smoothDeceleration(pitchOut, pitch, roll - MAX_AUTOPILOT_MOVEMENT); // move backward
+      break;
+    default:
+      break;
+  }
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -174,115 +290,14 @@ void smoothDeceleration(int outputPin, int currentSpeed, int finalSpeed) {
   }
 }
 
-
-
-
-
-
-
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-// Forward RC Signals To Flight Controller
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-void forwardRCSignalsToFlightController() {
-  throttle = pulseIn(throttleIn, HIGH);
-  roll = pulseIn(rollIn, HIGH);
-  pitch = pulseIn(pitchIn, HIGH);
-  yaw = pulseIn(yawIn, HIGH);
-
-  analogWrite(throttleOut, map(throttle, 0, 2000, 0, 255));
-  analogWrite(rollOut, map(roll, 0, 2000, 0, 255));
-  analogWrite(pitchOut, map(pitch, 0, 2000, 0, 255));
-  analogWrite(yawOut, map(yaw, 0, 2000, 0, 255));
-
-  if (serialMonitorIsOpen) printReceiverInputToConsole();
-}
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-// Run Auto Pilot Loop: determine sensor with closest measurement
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-int runAutoPilot() {
-  unsigned long distanceMeasurement;
-  unsigned long doubleCheckDistanceMeaseurement;
-  int temporaryMinimum;
-  int temporaryAvoidanceDirection;
-  boolean avoidanceDirectionWasSet;
-  
-  while (true) {
-    temporaryMinimum = OUTER_DISTANCE_THRESHOLD_FOR_SENSORS_IN_CM;
-    temporaryAvoidanceDirection = -1;
-    avoidanceDirectionWasSet = false;
-    for (int sensorIndex = 0; sensorIndex < 4; sensorIndex++) {
-      
-      forwardRCSignalsToFlightController();
-      
-      /*  Do a burst of 5 ping measurements, discard out of range pings and return median in microseconds */
-      distanceMeasurement = sensors[sensorIndex].ping_median(NUMBER_OF_SONAR_BURSTS) / US_ROUNDTRIP_IN;
-
-      forwardRCSignalsToFlightController();
-      
-      /* Failsafe mechanism for exiting autopilot */
-      if (!checkAuxForAutoPilotSignal()) break;
-      
-      /* Check if distanceMeasurement is out of range and therefore throwaway */
-      if (distanceMeasurement == 0) continue;
-      
-      /* Update temporaryAvoidanceDirection and resulting direction */
-      if (distanceMeasurement < temporaryMinimum) {
-        temporaryMinimum = distanceMeasurement;
-        temporaryAvoidanceDirection = sensorIndex;
-        avoidanceDirectionWasSet = true;
-      }     
-    } // end for
-    
-    /* Failsafe mechanism for exiting autopilot */
-    if (!checkAuxForAutoPilotSignal()) {
-       if (serialMonitorIsOpen) Serial.println("Break Out");
-      break;
-    }
-    
-    /* Avoid a collision by moving the quadcopter is a safe direction */
-    /* shift two sensor i.e the opposite direction of the triggering sensor */
-    if (avoidanceDirectionWasSet) {
-      if (serialMonitorIsOpen) Serial.println("Avoidance Was Called");
-      
-      doubleCheckDistanceMeaseurement = sensors[temporaryAvoidanceDirection].ping_median(NUMBER_OF_SONAR_BURSTS) / US_ROUNDTRIP_IN;
-      if (doubleCheckDistanceMeaseurement < temporaryMinimum) {
-        forwardCollisionAvoidanceMovementToFlightControler((temporaryAvoidanceDirection + 2) % 4);
-      }
-    }
-  } // end while
-}
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-// Forward Collision Avoidance Movement To The Flight Controler
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-void forwardCollisionAvoidanceMovementToFlightControler(int avoidanceDirection) {
-  switch (avoidanceDirection) {
-      case 0:
-        smoothAcceleration(rollOut, roll, roll + CAPACITY_OF_AUTOPILOT_MOVEMENTS); // move left
-        break;
-      case 1:
-        smoothAcceleration(pitchOut, pitch, pitch + CAPACITY_OF_AUTOPILOT_MOVEMENTS); // move forward
-        break;
-      case 2:
-        smoothDeceleration(rollOut, roll, roll - CAPACITY_OF_AUTOPILOT_MOVEMENTS); // move right
-        break;
-      case 3:
-        smoothDeceleration(pitchOut, pitch, roll - CAPACITY_OF_AUTOPILOT_MOVEMENTS); // move backward
-        break;
-      default:
-        break;
-    }
-}
-
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 // Main Loop
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 void loop() {
-  if (checkAuxForAutoPilotSignal()) {
+  if (isAutoPilotMode()) {
     runAutoPilot();
   } else {
     forwardRCSignalsToFlightController();
   }
 }
+
